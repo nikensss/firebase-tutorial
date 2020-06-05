d66d68d61b2c51759d7fcdb1dc9ae1c0b5126c1e
Ricard Mallafré López <rikitzzz@gmail.com>
2020-06-05 09:26:12 +0200

    initial commit

---
+++ public/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const app = firebase.app();
+  console.log({ app });
+
+  const db = firebase.firestore();
+
+  const myPost = db.collection('posts').doc('firstpost');
+  myPost.get().then((doc) => {
+    const data = doc.data();
+    console.log(data);
+    document.write(data.title + '<br>');
+    document.write(data.views);
+  });
+});
+
+function googleLogin() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      const user = result.user;
+      document.write(`Hello, ${user.displayName}`);
+      console.log(user);
+    })
+    .catch(console.error);
+}
